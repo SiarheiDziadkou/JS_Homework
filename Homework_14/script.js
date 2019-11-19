426391b1158383ce5d24f9ae0b4d2c9e3d1ff076
Siarhei Dziadkou <dedkov.blr@gmail.com>
2019-11-20 00:01:34 +0300

    HW_14

---
+++ Homework_14/script.js
@@ -0,0 +1,76 @@
+function validateInput() {
+    var lineX = document.getElementById('lengthX').value;
+    var lineY = document.getElementById('lengthY').value;
+    var regNum = /^([1-9]|10)$/;
+    if (regNum.exec(lineX) == null) {
+        alert('Введите в строку X: число от 1 до 10')
+        document.getElementById('lengthX').value = '';
+        document.getElementById('myButt').disabled = true;
+    } else if (regNum.exec(lineY) == null) {
+        alert('Введите в строку Y: число от 1 до 10')
+        document.getElementById('lengthY').value = '';
+        document.getElementById('myButt').disabled = true;
+    } else {
+        document.getElementById('myButt').disabled = false;
+        drawChessBoard();
+    }
+}
+
+function checkIfInputsEmpty() {
+    var lineX = document.getElementById('lengthX').value;
+    var lineY = document.getElementById('lengthY').value;
+    if ((lineY === '' || lineY == null) || (lineX === '' || lineX == null)) {
+        document.getElementById('myButt').disabled = true;
+    } else {
+        document.getElementById('myButt').disabled = false;
+    }
+}
+
+document.getElementById('myButt').onclick = function() {
+    validateInput();
+
+}
+
+document.getElementById('lengthX').onkeyup = function() {
+    checkIfInputsEmpty()
+}
+
+document.getElementById('lengthY').onkeyup = function() {
+    checkIfInputsEmpty()
+}
+
+function drawChessBoard() {
+    var chessBoard = document.getElementById("chessBoard");
+    chessBoard.innerHTML = '';
+    var x = document.getElementById('lengthX').value;
+    var y = document.getElementById('lengthY').value;
+
+    for (var i = 0; i < y; i++) {
+        var row = chessBoard.appendChild(document.createElement("div"));
+        for (var j = 0; j < x; j++) {
+            var column = document.createElement("span");
+            column.className = "cell"
+            if (i % 2 === 0) {
+                j % 2 === 0 ? column.classList.add("white") : column.classList.add("black");
+            } else {
+                j % 2 === 0 ? column.classList.add("black") : column.classList.add("white");
+            }
+            row.appendChild(column);
+        }
+    }
+}
+
+document.getElementById('chessBoard').onclick = function(event) {
+    var target = event.target;
+    if (target.tagName.toLowerCase() != 'span') { return }
+    var cells = document.getElementsByClassName("cell");
+    for (var i = 0; i < cells.length; i++) {
+        if (cells[i].classList.value.includes("white")) {
+            cells[i].classList.remove('white');
+            cells[i].classList.add('black');
+        } else {
+            cells[i].classList.remove('black');
+            cells[i].classList.add('white');
+        }
+    }
+}
